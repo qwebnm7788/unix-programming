a92a60c143582dff34409cd67b4cae42406611f7
Jaewon Choi <qwebnm7788@naver.com>
2017-11-19 14:42:33 +0900

    #2 complete

--- smallsh.c
+++ smallsh.c
@@ -1,7 +1,7 @@
 /*
    12131619 컴퓨터공학과 최재원
-   2017.11.5
-   Shell Project #1
+   2017.11.19
+   Shell Project #2
 */
 
 #include <stdio.h>
@@ -11,6 +11,7 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <signal.h>
 
 #define MAX_CMD_ARG 10
 #define MAX_CMD_GRP 10
@@ -26,11 +27,24 @@ void fatal(char* str);
 void execute_cmdline(char* cmd);
 int  parse_cmdgrp(char* cmdGrp);
 int  makelist(char* s, const char* delimiters, char** list, int MAX_LIST);
+void sigchld_handler(int signo);
 
 int main(int argc, char* argv[]) {
 	pid_t pid;
 	int token;
+	struct sigaction act;
 
+	//install sigchld handler
+	sigemptyset(&act.sa_mask);
+	act.sa_handler = sigchld_handler;
+	act.sa_flags = SA_RESTART;
+	sigaction(SIGCHLD, &act, NULL);
+
+	//SIGINT, SIGQUIT, SIGTSTP signal을 무시하도록 설정
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGTTOU, SIG_IGN);				//tcsetpgrp이 background process에서 호출되는 경우 SIGTTOU signal이 날아감 (왜 fork한게 background process group이 되었냐면 setpgid로 새로운 프로세스 그룹을 만들었기 때문)
 	//시작과 동시에는 home directory에 존재한다.
 	if(chdir(getenv("HOME")) == -1) {
 		fatal("shell init fail");
@@ -41,6 +55,7 @@ int main(int argc, char* argv[]) {
 		fputs(prompt, stdout);
 		fgets(cmdline, BUFSIZ, stdin);
 		cmdline[strlen(cmdline) - 1] = '\0';
+
 		if(!strcmp(cmdline, "exit")) {
 			//exit은 fork를 사용하지 않고 해당 쉘에서 곧바로 종료
 			printf("Good bye~\n");
@@ -98,8 +113,22 @@ void execute_cmdline(char* cmdline) {
 	for(i = 0; i < count; ++i) {
 		background = parse_cmdgrp(cmdGrps[i]);			//주어진 cmdGrps에 나누어진 명령을 다시 추가 argument로 나누어 cmdVector에 저장한다. (반환값은 background의 경우 1)
 		switch(pid = fork()) {
-			case -1: fatal("fork error");
-			case  0: execvp(cmdVector[0], cmdVector); fatal("execvp error");		//자식 프로세스는 주어진 프로세스를 수행한다.
+			case -1: 
+				fatal("fork error");
+			case  0:
+				setpgid(0, 0);			//pgid를 자신의 pid와 동일하게 설정 -> 새로운 프로세스 그룹이 생성된다.
+
+				//SIGINT, SIGQUIT, SIGTSTP 을 다시 default handler로 돌려놓는다. -> 이 것에 죽지않는 것은 shell만 되도록
+				signal(SIGINT, SIG_DFL);
+				signal(SIGQUIT, SIG_DFL);
+				signal(SIGTSTP, SIG_DFL);
+
+				//foreground인 경우 잠시동안 터미널 제어권을 가져가도록 한다.
+				if(background == 0)
+					tcsetpgrp(STDIN_FILENO, getpgid(0));			//자신의 부모(위의 setpgid로 자신으로 변경되어 있음)가 터미널 제어권을 갖는다.
+
+				execvp(cmdVector[0], cmdVector); //자식 프로세스는 주어진 프로세스를 수행한다.
+				fatal("execvp error");
 			default: 
 				if(background == 1) {
 					//background task의 경우 waitpid의 WNOHANG 옵션을 이용하여 대기하지 않는다.
@@ -109,6 +138,9 @@ void execute_cmdline(char* cmdline) {
 					//단순히 wait으로 대기하게 되면 임의의 자식이 reap될 수 있기 때문에 waitpid에 pid를 넘겨주고 세번째 인자는
 					//0을 넘겨주어 기존 특정 프로세스를 기다리는 wait의 효과를 내어주었다.
 					result = waitpid(pid, &status, 0);
+
+					//foreground task가 종료된 후 다시 터미널 제어권을 가져온다.	(standard input -> 0)
+					tcsetpgrp(STDIN_FILENO, getpgid(0));
 				}else {
 					fatal("execute_cmdline error");
 				}
@@ -159,4 +191,10 @@ int makelist(char* s, const char* delimiters, char** list, int MAX_LIST) {
 	}
 
 	return numTokens;
+}
+
+
+//SIGCHLD signal이 오면 그 때 존재하는 모든 zombie process를 reap해준다.
+void sigchld_handler(int signo) {
+	while(waitpid(-1, NULL, WNOHANG) > 0);
 }
